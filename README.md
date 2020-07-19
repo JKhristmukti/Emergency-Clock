acc2b0d8dd8a30097666ede76cf07b0028bb1b75
Jason.A.Khristmukti <64148391+JKhristmukti@users.noreply.github.com>
2020-07-19 20:14:47 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Emergency-Clock
+# p5.play-boilerplate
+Boiler plate for p5.play
---
+++ sketch.js
@@ -0,0 +1,59 @@
+var hr,mn,sc;
+
+var centre;
+
+var hrhand,mnHand,scHand;
+
+var hrAngle,mnAngle,scAngle;
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() {
+  background(255,255,255);
+  angleMode(DEGREES);
+
+  push();
+  ellipseMode(RADIUS);
+  fill("white");
+  ellipse(200,200,100,100);
+  pop();
+
+
+  hr = hour();
+  mn = minute();
+  sc = second();
+  
+  hrAngle = map(hr,0,60,0,360);
+  
+  mnAngle = map(mn,0,60,0,360);
+  
+  scAngle = map(sc,0,60,0,360);
+
+  push();
+  rotate(scAngle);
+  stroke("blue");
+  strokeWeight(7);
+  line(200,120,200,200);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  stroke("green");
+  strokeWeight(7);
+  line(200,140,200,200);
+  pop();
+
+  push();
+  rotate(scAngle);
+  stroke("red");
+  strokeWeight(7);
+  line(200,180,200,200);
+  pop();
+
+  //textSize(50);
+  //text("Time: " + hr + ": " + mn + ": " + sc, 50,100);
+
+  drawSprites();
+}
